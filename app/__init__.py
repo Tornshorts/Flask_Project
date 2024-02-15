37e7481c5d04a651687d59f8a66411d62228d0b6
Tornshorts <kawewaalex@gmail.com>
2024-02-15 14:29:44 +0300

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+login = LoginManager(app)
+login.login_view = 'login'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models
